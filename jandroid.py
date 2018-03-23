d1ff7e0a9d755747085e45bc9af9f5c444cd9441
Jan <janwendel2@gmail.com>
2018-03-23 08:07:42 +0100

    role setup

--- jandroid.py
+++ jandroid.py
@@ -6,25 +6,60 @@ import random, pickle, codecs, urllib.request
 import logging
 from datetime import datetime
 
+from xp_db import *
+
 jd = Bot(command_prefix="!")
+my_bot.remove_command("help")
 
-@my_bot.event
+@jd.event
 async def on_message(ctx):
-    print(ctx.content)
-    words = {"ayy":"lmao","wew":"lad","ba dum":"tss","ur gay":"no u","he's a big guy":"For you."}
+    user_id = ctx.author.id
     content = ctx.content.lower()
-    s = ctx.content.lower().replace(" ","")
-    if content.replace(" ","") in [i.replace(" ","") for i in words]:
-        await ctx.channel.send(words[content])
-    await my_bot.process_commands(ctx)
 
-#Roll 1-100
-@my_bot.command()
-async def roll(ctx,limit = 101):
-    num = str(random.randrange(1,limit))
-    if len(num) > 1 and num[-1] == num[-2]:
-        return await ctx.channel.send(num+"\nChecked!")
-    else: return await ctx.channel.send(num)
+    #ROLL
+    if content == "roll":
+        roll_lim = 100
+        num = str(random.randrange(1,roll_lim))
+        if len(num) > 1 and num[-1] == num[-2]:
+            return await ctx.channel.send(num+"\nChecked!")
+        else: return await ctx.channel.send(num)
+    elif content[:4] == "roll":
+        try:
+            roll_lim = int(content.split(" ")[1])
+            num = str(random.randrange(1,roll_lim))
+            if len(num) > 1 and num[-1] == num[-2]:
+                return await ctx.channel.send("{} \n**Checked!**".format(num))
+            else: return await ctx.channel.send(num)
+        except ValueError:
+            pass
+
+    gangs_owners = [i[2] for i in gen_dump("gang_xp")]
+
+    elif content == "make_gang":
+        def create_gang(gang_name):
+            return ctx.guild.create_gang(name)
+        
+        words = content.split(" ")
+        if len(words) > 1:
+            role_name = " ".join(words)
+            new_role = create_gang(content)
+            role.id = new_role.id
+            members = (i[0] for i in gen_dump("membership"))
+            owners = (i[2] for i in gen_dump("gang_xp"))
+
+        pass
+
+    elif content == "join_gang":
+        pass
+
+    elif content == "leave_gang":
+        pass
+
+    await jd.process_commands(ctx)
+
+@jd.event
+async def on_ready():
+    print("READY!")
 
 
 
